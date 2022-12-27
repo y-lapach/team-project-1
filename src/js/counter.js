a87e7e3861bdfb432cc4a97063ab2e08fd37e406
Yana Lapach <y.lapach@gmail.com>
2022-12-27 03:06:39 +0100

    bug fixes

--- src/js/counter.js
+++ src/js/counter.js
@@ -1,11 +1,16 @@
+setTimeout(
+  function() 
+  {
+   
 $('.count').each(function () {
     $(this).prop('Counter',0).animate({
         Counter: $(this).text()
     }, {
-        duration: 4000,
+        duration: 2000,
         easing: 'swing',
         step: function (now) {
             $(this).text(Math.ceil(now));
         }
     });
 });
+}, 2000);
--- src/js/hero-title-typing.js
+++ src/js/hero-title-typing.js
@@ -1,4 +1,4 @@
-var content = 'ice cream made with passion';
+/*var content = 'ice cream made with passion';
 
 var ele = '<span>' + content.split('').join('</span><span>') + '</span>';
 
@@ -10,4 +10,40 @@ $(ele).hide().appendTo('h1').each(function (i) {
     }).animate({
         opacity: 1
     }, 100);
-});
+});*/
+
+const sleep = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
+
+const clearAndCache = (elements) => {
+  let cachedText = [];
+  for (let element of elements) {
+    cachedText.push(element.textContent);
+    element.textContent = "";
+  }
+  return cachedText;
+};
+
+const animateTypeSync = async (typeables, ms) => {
+  const cachedText = clearAndCache(typeables);
+
+  for (let i = 0; i < typeables.length; i++) {
+    for (let character of cachedText[i]) {
+      typeables[i].textContent += character;
+      await sleep(ms);
+    }
+  }
+};
+
+const animateTypeAsync = (typeables, ms) => {
+  const cachedText = clearAndCache(typeables);
+
+  Array.from(typeables).forEach(async (element, i) => {
+    for (let character of cachedText[i]) {
+      element.textContent += character;
+      await sleep(ms);
+    }
+  });
+};
+
+const elementsSync = document.getElementsByClassName("hero-title-1");
+animateTypeSync(elementsSync, 100);
