088b0f83ce0eed3b58c575e497e1920ff0625c0b
y-lapach <114833155+y-lapach@users.noreply.github.com>
2022-12-28 16:41:42 +0000

    Deploying to gh-pages from @ y-lapach/team-project-1@b7c5ed4b342ce0d15fddc13c94e283bfda219239 🚀

---
+++ presentation.35001818.js
@@ -0,0 +1,2 @@
+$(document).ready((function(){$(".dev-slider-wrapper").slick({arrows:!1,dots:!0,fade:!0,cssEase:"linear",speed:400,easing:"ease"})}));
+//# sourceMappingURL=presentation.35001818.js.map
